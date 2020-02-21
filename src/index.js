c7a14c8ad96bc1c73afc0093e7385b3660674ab3
bohdansemyvolos <b.semivolos@agilites.com>
2020-02-21 10:45:25 +0200

    feat: implement towel-sort task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+    var result = [];
+    if (matrix != undefined) {
+    for (var i = 0; i < matrix.length; i++) {
+        if (i % 2 === 0) {
+            for (var j = 0; j < matrix[i].length; j++) {
+                result.push(matrix[i][j]);
+            }
+        } else {
+            for (var j = matrix[i].length - 1; j >= 0; j--) {
+                result.push(matrix[i][j]);
+            }
+        }
+    }
+}
+    return result;
 }
